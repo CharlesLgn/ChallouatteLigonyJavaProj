3d9706a74827ac22d7843c0a0de8127cc20ea83e
cyril.challouatte <cyrilchallouatte@yahoo.fr>
2018-09-27 14:33:09 +0200

    Ajout son

---
+++ src/method/JouerSon.java
@@ -0,0 +1,19 @@
+package method;
+
+public class JouerSon {
+
+    public static void jouerson (String morse) throws Exception{
+        for (char item : morse.toCharArray()) {
+            switch (item){
+                case '-':
+                    Beep.beepLong();
+                    break;
+                case '.':
+                    Beep.beepCourt();
+                    break;
+                case ' ':
+                    Beep.pauseEntreLettre();
+            }
+        }
+    }
+}
