59253f8b5d5ee06fb408e1717fdf272587f069c4
charles ligony <c-ligony@efluid.fr>
2018-11-10 09:12:29 +0100

    final

--- src/com/main/MainJavaFx.java
+++ src/com/main/MainJavaFx.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import resource.lang.Lang;
